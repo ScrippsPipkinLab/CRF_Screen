da890e2a9e32e989ad292cc15f65d52d2274d900
YoloDHT <huitiandiao@gmail.com>
2019-05-15 17:25:57 -0400

    update

--- 0.1_Codes_Invivo/2_1_candidate_interaction.R
+++ 0.1_Codes_Invivo/2_1_candidate_interaction.R
@@ -2,6 +2,7 @@
 # Author: Huitian (Yolanda) Diao
 # May 15th, 2019
 # For t-test with percentiles
+# Reference: https://rdrr.io/bioc/TCGAbiolinks/man/getAdjacencyBiogrid.html
 
 ########## Libraries ##########
 library(dplyr)
